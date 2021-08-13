593af365b30760c78f4b1cb8f67eececc146247e
Jun Feng <alexfengau@gmail.com>
2021-08-14 08:56:43 +1000

    init project

---
+++ src/main/java/com/yieldbroker/sportcentre/reservation/SportCentreReservationApplication.java
@@ -0,0 +1,13 @@
+package com.yieldbroker.sportcentre.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportCentreReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportCentreReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yieldbroker/sportcentre/reservation/SportCentreReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yieldbroker.sportcentre.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportCentreReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
